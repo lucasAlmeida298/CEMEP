38ba75ba5bc4b7e03d189a20f81a37dcc40bbaac
lucasAlmeida298 <dealmeidalucas298@gmail.com>
2026-03-05 15:02:29 -0300

    Delete DS II - Fabiana/1ºBimestre/aula2502 directory

--- DS II - Fabiana/1ºBimestre/aula2502/exemplo1.py
+++
@@ -1,9 +0,0 @@
-#função type: usada para verificar o tipo de uma variável em Python
-a = 10
-b = 1.5
-nome = ("Lucas")
-print ("O tipo da variável a é: ",type(a))
-#__name__ é uma variável especial que indica o nome do módulo que está sendo executado
-print ("O tipo de variável a é: ",type(a).__name__)
-print ("O tipo de variável b é: ",type(b).__name__)
-print ("O tipo de variável nome é: ",type(nome).__name__)
