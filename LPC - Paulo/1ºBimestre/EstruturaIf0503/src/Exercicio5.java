b7ee801842a966ea0289bc44ddc5e4fab6408331
lucasAlmeida298 <dealmeidalucas298@gmail.com>
2026-03-05 17:18:03 -0300

    Edição pendrive

---
+++ LPC - Paulo/1ºBimestre/EstruturaIf0503/src/Exercicio5.java
@@ -0,0 +1,11 @@
+public class Exercicio5 {
+    public static void main(String[] args) {
+        int i;
+        i = 0;
+        while (i<200) {
+            i=i+10;
+            System.out.println("n=" +i);
+        }
+
+    }
+}
---
+++ LPC - Paulo/1ºBimestre/EstruturaIf0503/src/Exercicio6.java
@@ -0,0 +1,8 @@
+public class Exercicio6 {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i=0; i<=200; i=i+10) {
+            System.out.println("n=" + i);
+        }
+    }
+}
