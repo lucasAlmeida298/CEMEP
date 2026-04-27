926ff5f4cd05835146527282086960cf567db136
Lucas <dealmeidalucas298@gmail.com>
2026-04-27 17:35:32 -0300

    nm

---
+++ LUCAS 2ºW/DS II - Fabiana/1ºBimestre/aula2704/form1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <table border="1" align="center" width="50%">
+        <tr>
+            <td align="center">
+                <h1>Exercício 1</h1>
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <h3>Paulínia, 27 de Abril de 2026</h3>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <h3>Calculadora:</h3>
+                <h3>(1) Soma <br>
+                    (2) Subtração <br>
+                    (3) Divisão <br>
+                    (4) Multiplicação </h3>
+                <h3>Escolha a operação: </h3><input type="text" name="operacao"
+                placeholder="Digite a operação desejada" size="40">
+            </td>
+        </tr>
+</body>
+</html>
