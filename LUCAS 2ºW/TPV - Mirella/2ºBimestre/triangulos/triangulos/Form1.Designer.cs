dda15b58440d20bdab038998c9eedfe9249ba207
Lucas <dealmeidalucas298@gmail.com>
2026-04-28 15:38:49 -0300

    tpv

---
+++ LUCAS 2ºW/TPV - Mirella/2ºBimestre/triangulos/triangulos/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace triangulos
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtLado1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtLado2 = new System.Windows.Forms.TextBox();
+            this.txtLado3 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lblT1 = new System.Windows.Forms.Label();
+            this.lblT2 = new System.Windows.Forms.Label();
+            this.lblT3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.btnT = new System.Windows.Forms.Button();
+            this.btnL = new System.Windows.Forms.Button();
+            this.btnS = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lbl1.Location = new System.Drawing.Point(47, 41);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(52, 16);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "lado A";
+            // 
+            // txtLado1
+            // 
+            this.txtLado1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLado1.Location = new System.Drawing.Point(105, 38);
+            this.txtLado1.Name = "txtLado1";
+            this.txtLado1.Size = new System.Drawing.Size(100, 23);
+            this.txtLado1.TabIndex = 1;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lbl2.Location = new System.Drawing.Point(254, 41);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(50, 16);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "lado B";
+            // 
+            // txtLado2
+            // 
+            this.txtLado2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLado2.Location = new System.Drawing.Point(310, 38);
+            this.txtLado2.Name = "txtLado2";
+            this.txtLado2.Size = new System.Drawing.Size(100, 23);
+            this.txtLado2.TabIndex = 3;
+            // 
+            // txtLado3
+            // 
+            this.txtLado3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLado3.Location = new System.Drawing.Point(537, 38);
+            this.txtLado3.Name = "txtLado3";
+            this.txtLado3.Size = new System.Drawing.Size(100, 23);
+            this.txtLado3.TabIndex = 5;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lbl3.Location = new System.Drawing.Point(479, 41);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(52, 16);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "lado C";
+            // 
+            // lblT1
+            // 
+            this.lblT1.AutoSize = true;
+            this.lblT1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblT1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblT1.Location = new System.Drawing.Point(254, 87);
+            this.lblT1.Name = "lblT1";
+            this.lblT1.Size = new System.Drawing.Size(218, 136);
+            this.lblT1.TabIndex = 7;
+            this.lblT1.Text = resources.GetString("lblT1.Text");
+            this.lblT1.Visible = false;
+            // 
+            // lblT2
+            // 
+            this.lblT2.AutoSize = true;
+            this.lblT2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblT2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblT2.Location = new System.Drawing.Point(257, 248);
+            this.lblT2.Name = "lblT2";
+            this.lblT2.Size = new System.Drawing.Size(215, 153);
+            this.lblT2.TabIndex = 8;
+            this.lblT2.Text = resources.GetString("lblT2.Text");
+            this.lblT2.Visible = false;
+            // 
+            // lblT3
+            // 
+            this.lblT3.AutoSize = true;
+            this.lblT3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblT3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblT3.Location = new System.Drawing.Point(265, 435);
+            this.lblT3.Name = "lblT3";
+            this.lblT3.Size = new System.Drawing.Size(207, 119);
+            this.lblT3.TabIndex = 9;
+            this.lblT3.Text = "Triângulo escaleno é um\r\npolígono que possui os três\r\nlados com medidas diferente" +
+    "s.\r\nAssim sendo os triângulos\r\nescalenos não são polígonos\r\nregulares e nem poss" +
+    "uem eixo\r\ncongruentes";
+            this.lblT3.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Image = global::triangulos.Properties.Resources.trinaguloEscaleno2;
+            this.pictureBox1.Location = new System.Drawing.Point(501, 435);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pic2.Image = global::triangulos.Properties.Resources.trianguloIsoceles3;
+            this.pic2.Location = new System.Drawing.Point(501, 247);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(222, 154);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 10;
+            this.pic2.TabStop = false;
+            this.pic2.Visible = false;
+            // 
+            // pic1
+            // 
+            this.pic1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pic1.Image = global::triangulos.Properties.Resources.triânguloEquilátero1;
+            this.pic1.Location = new System.Drawing.Point(503, 87);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(220, 136);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 6;
+            this.pic1.TabStop = false;
+            this.pic1.Visible = false;
+            // 
+            // btnT
+            // 
+            this.btnT.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnT.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btnT.Location = new System.Drawing.Point(39, 87);
+            this.btnT.Name = "btnT";
+            this.btnT.Size = new System.Drawing.Size(142, 237);
+            this.btnT.TabIndex = 12;
+            this.btnT.Text = "Triângulo";
+            this.btnT.UseVisualStyleBackColor = true;
+            this.btnT.Click += new System.EventHandler(this.btnT_Click);
+            // 
+            // btnL
+            // 
+            this.btnL.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnL.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btnL.Location = new System.Drawing.Point(39, 334);
+            this.btnL.Name = "btnL";
+            this.btnL.Size = new System.Drawing.Size(142, 113);
+            this.btnL.TabIndex = 13;
+            this.btnL.Text = "Limpar";
+            this.btnL.UseVisualStyleBackColor = true;
+            this.btnL.Click += new System.EventHandler(this.btnL_Click);
+            // 
+            // btnS
+            // 
+            this.btnS.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnS.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btnS.Location = new System.Drawing.Point(39, 467);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(142, 113);
+            this.btnS.TabIndex = 14;
+            this.btnS.Text = "Sair";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(750, 592);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.btnL);
+            this.Controls.Add(this.btnT);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.lblT3);
+            this.Controls.Add(this.lblT2);
+            this.Controls.Add(this.lblT1);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.txtLado3);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.txtLado2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txtLado1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form";
+            this.Text = "Tipos de Triângulos";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtLado1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtLado2;
+        private System.Windows.Forms.TextBox txtLado3;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lblT1;
+        private System.Windows.Forms.Label lblT2;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.Label lblT3;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnT;
+        private System.Windows.Forms.Button btnL;
+        private System.Windows.Forms.Button btnS;
+    }
+}
+
---
+++ LUCAS 2ºW/TPV - Mirella/2ºBimestre/triangulos/triangulos/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace triangulos
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnS_Click(object sender, EventArgs e)
+        {
+            //declaração de variável da caixa de diálogo
+            DialogResult result;
+            result = MessageBox.Show("Deseja sair mesmo?", "Saída", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(result==DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnT_Click(object sender, EventArgs e)
+        {
+            //declarar variáveis
+
+            double a, b, c;
+
+            //conversão das variáveis
+
+            a = Convert.ToDouble(txtLado1.Text);
+            b = Convert.ToDouble(txtLado2.Text);
+            c = Convert.ToDouble(txtLado3.Text);
+
+            //lógica cálculo ver se é triângulo
+                           // operador lógico E no C# é &&
+                           // operador lógico OU no C# é ||
+            // a<b+c
+            // b<a+c
+            // c<a+b
+
+            if (a<b+c && b<a+c && c<a+b)
+            {
+                MessageBox.Show("Representam os lados de um triângulo", "Valores válidos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            // Triângulo equilátero
+        }
+
+        private void btnL_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
