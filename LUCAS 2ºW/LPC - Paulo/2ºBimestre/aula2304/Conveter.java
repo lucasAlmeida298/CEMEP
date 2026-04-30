cfa217e184d621e89583c107cf0b1134dda48c39
Lucas <dealmeidalucas298@gmail.com>
2026-04-30 16:24:17 -0300

    jav=va

--- LUCAS 2ºW/LPC - Paulo/2ºBimestre/aula2304/Conveter.java
+++ LUCAS 2ºW/LPC - Paulo/2ºBimestre/aula2304/Conveter.java
@@ -1,11 +1,26 @@
 public class Conveter {
     public double convertDolar (double a){
-        return a*0.2;
+        return a/4.98;
     }
     public double convertEuro (double a){
-        return a*0.17;
+        return a/5.84;
     }
     public double convertIene (double a){
-        return a*31.8;
+        return a/0.032;
     }
+    public double convesorGeral(double valorReais, int opcao){
+        double valorConvertido=0;
+        if(opcao==1){
+            valorConvertido=valorReais/4.98;
+        }
+        else if (opcao==2){
+            valorConvertido=valorReais/5.84;
+        }
+        else if (opcao==3){
+            valorConvertido=valorReais/0.032;
+        }
+
+        return valorConvertido;
+
+  }
 }
